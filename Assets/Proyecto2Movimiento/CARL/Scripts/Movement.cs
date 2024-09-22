d6de9dde6c86ec5c601e3dee37f415ac381e063c
JuanJAreiza <105412915+JuanJAreiza@users.noreply.github.com>
2024-09-22 14:59:49 -0500

    Animaciones y montaje de Carl

---
+++ Assets/Proyecto2Movimiento/CARL/Scripts/Movement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using CallbackContext = UnityEngine.InputSystem.InputAction.CallbackContext;
+
+public class Carl_Movement : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+
+    [SerializeField] private Transform cameraTransform;
+
+    [SerializeField] private VectorDamper_Carl motionVector = new VectorDamper_Carl(true);
+
+    private int velXID, velYID;
+
+    public void Awake()
+    {
+        velXID = Animator.StringToHash("VelX");
+        velYID = Animator.StringToHash("VelY");
+    }
+
+    public void Move(CallbackContext ctx)
+    {
+        Vector2 direction = ctx.ReadValue<Vector2>();
+
+        motionVector.TargetValue = direction;
+
+    }
+
+    public void ToggleSprint(CallbackContext ctx)
+    {
+        bool val = ctx.ReadValueAsButton();
+        motionVector.Clamp = !val;
+    }
+
+    private void Update()
+    {
+        motionVector.Update();
+
+        anim.SetFloat(velXID, motionVector.CurrentValue.x);
+        anim.SetFloat(velYID, motionVector.CurrentValue.y);
+    }
+
+    private void OnAnimatorMove()
+    {
+        float interpolator = Mathf.Abs(Vector3.Dot(cameraTransform.forward, transform.up));
+        Vector3 forward = Vector3.Lerp(cameraTransform.forward, cameraTransform.up, interpolator);
+        Vector3 projected = Vector3.ProjectOnPlane(forward, transform.up);
+        Quaternion rotation = Quaternion.LookRotation(projected, transform.up);
+        anim.rootRotation = Quaternion.Slerp(anim.rootRotation, rotation, motionVector.CurrentValue.magnitude);
+        anim.ApplyBuiltinRootMotion();
+    }
+}
