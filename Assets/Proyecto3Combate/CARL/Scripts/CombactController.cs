aa34760f99df5980b4378d1b6c925356e091a528
JuanJAreiza <105412915+JuanJAreiza@users.noreply.github.com>
2024-10-13 21:27:47 -0500

    Carl Entrega 3

--- Assets/Proyecto3Combate/CARL/Scripts/CombactController.cs
+++ Assets/Proyecto3Combate/CARL/Scripts/CombactController.cs
@@ -9,8 +9,8 @@ using CallbackContext = UnityEngine.InputSystem.InputAction.CallbackContext;
 public class CombactController : MonoBehaviour
 {
     [SerializeField] int WeaponMaxIndex;
-    /* [SerializeField] GameObject swordMesh;*/
     private Animator anim;
+    private int comboIndex = 0;
 
     void Awake()
     {
@@ -22,26 +22,48 @@ public class CombactController : MonoBehaviour
     }
 
     public void ChangeWeapon(CallbackContext ctx){
+        if (!anim.GetBool("canChange")) return;
+
         bool val = ctx.performed;
         if (val){
-            anim.SetInteger("Weapon", anim.GetInteger("Weapon")+1);
-            if(anim.GetInteger("Weapon")> WeaponMaxIndex) anim.SetInteger("Weapon", 1);
+            int currentWeapon = anim.GetInteger("Weapon");
+            currentWeapon = (currentWeapon % WeaponMaxIndex) + 1; // Alterna entre 1 y 2
+            anim.SetInteger("Weapon", currentWeapon);
+
             anim.SetTrigger("ChangeWeapon");
-            /*InstantiateWeapon(anim.GetInteger("Weapon"));*/
+            Debug.Log("Cambio de arma, nueva arma: " + currentWeapon);
         }
     }
 
     public void Attack(CallbackContext ctx){
-        if(!anim.GetBool("canAttack")) return;
+        if (!anim.GetBool("canAttack")) return; // No permitas ataques si no se puede atacar
+
         bool val = ctx.performed;
-        if(val){
+        if (val)
+        {
             anim.SetTrigger("Attack");
+
+            // Si el ataque se ha realizado correctamente, avanza el combo
+            comboIndex++;
+            anim.SetInteger("ComboIndex", comboIndex); // Actualiza el Animator
+
+            // Bloquea el ataque hasta que el combo avance
             anim.SetBool("canAttack", false);
-        } 
+            Debug.Log("Ataque realizado, comboIndex: " + comboIndex);
+        }
     }
 
     public void OnAttackEnding(){
+        // Al final de cada ataque, permite seguir atacando y cambiar de paradigma
         anim.SetBool("canAttack", true);
+
+        // Resetea el combo si llega al máximo de ataques
+        if (comboIndex >= 5) // Máximo de combo para Punch es 5, para Kick sería otro valor
+        {
+            comboIndex = 0;
+        }
+
+        Debug.Log("Ataque terminado, canAttack activado.");
     }
 
     public void OnWeaponChanged(int value){
@@ -50,18 +72,4 @@ public class CombactController : MonoBehaviour
         else Result = true;
         anim.SetBool("canChange", Result);
     }
-
-    /*
-    public void InstantiateWeapon(int Weapon){
-        switch(Weapon){
-            default: break;
-            case 1:
-                swordMesh.SetActive(false);
-                break;
-            case 2:
-                swordMesh.SetActive(true);
-                _VFXUpdater.ToggleSword(1,0, swordMesh);
-                break;
-        }
-    }*/
 }
